547f3014ec3e77feea67751968fb6cd227c239fa
lagofelipe <lagofelipe@gmail.com>
2020-09-10 11:34:06 -0500

    submission for deadline

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -5,11 +5,34 @@ class Movie
 
   def initialize(title)
     @title = title
+
+
     self.class.all << self
   end
 
+  
+  def reviews
+    #returns an array of all the Review instances for the Movie
+    Review.all.select {|reviews|reviews.movie == self}
+  end
+
+  def reviewers 
+    #returns an array of all of the Viewer instances that reviewed the Movie
+      self.reviews.collect {|reviews|reviews.viewer}
+  end
+  
+  
+  def self.highest_rated
+
+    
+  end
+
+  
+  
   def self.all
     @@all
   end
+
+
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -9,8 +9,20 @@ end
 # Insert code here to run before hitting the binding.pry
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
+m1 = Movie.new("Apolo")
+m2 = Movie.new("MIB")
+m3 = Movie.new("500")
 
+v1 = Viewer.new("Big Bob")
+v2 = Viewer.new("Super Marcus")
+v3 = Viewer.new("Thickdaddy1000")
 
+#Review  (viewer, movie, rating)
+
+r1 = Review.new(v1, m1, "6")
+r2 = Review.new(v2, m2, "4")
+r3 = Review.new(v3, m3, "9")
+r4 = Review.new(v1, m3, "8")
 
 
 
